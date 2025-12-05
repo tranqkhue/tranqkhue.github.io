2b0a4b05860d8a4104823185c2e81914519064c7
tranqkhue <35965876+tranqkhue@users.noreply.github.com>
2025-12-05 02:17:41 +0000

    Deploying to gh-pages from @ tranqkhue/tranqkhue.github.io@ec6be27509e1c12ff6d3a4980a64f28a79644877 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "tranqkhue/tranquockhue.github.io",
+    "data-repo": "tranqkhue/tranqkhue.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
